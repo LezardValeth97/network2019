fa16781581a9a81a6047655ede07c832c1330553
lezardvaleth <themasterofwind1997@gmail.com>
2019-03-20 18:52:32 +0700

    Reupload practical 4 C

--- 04.practical.work.client.setup.c
+++ 04.practical.work.client.setup.c
@@ -9,12 +9,12 @@
 #include <netinet/in.h>
 
 
-int main(int argc, char const *argv[])
+int main(int argc, char **argv)
 {
 	struct sockaddr_in saddr;
 	struct hostent *h;
 	int sockfd;
-	unsigned short port = 8554;
+	unsigned short port = 8784;
 
 	char hostname[256];
 	if(argc == 1){
@@ -26,6 +26,10 @@ int main(int argc, char const *argv[])
 		printf("Too many arguments.\n");
 		return -1;
 	}
+	else if(argc < 2){
+		printf("Provide a hostname to resolve \n");
+		return 1;
+	}
 
 	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
 		printf("Error creating socket \n");
@@ -33,7 +37,7 @@ int main(int argc, char const *argv[])
 		return -1;
 	}
 
-	if((h = gethostbyname("ict.usth.edu.vn")) == NULL) {
+	if((h = gethostbyname(hostname)) == NULL) {
 		printf("Unknown host \n");
 		close(sockfd);
 		return -1;
@@ -42,20 +46,28 @@ int main(int argc, char const *argv[])
 	memset(&saddr, 0,sizeof(saddr));
 	saddr.sin_family = AF_INET;
 	memcpy((char *) &saddr.sin_addr.s_addr, h->h_addr_list[0], h->h_length);
-	struct hostent *gethost = gethostbyname(hostname); // get Internet hostname
-	struct in_addr **IP;
-	IP = (struct in_addr **)gethost -> h_addr_list; // get all the address
 
+	struct in_addr **IP;
+	IP = (struct in_addr **)h -> h_addr_list; // get all the address
+	struct in_addr ip = *IP[0];
+	printf("%s\n", inet_ntoa(ip));
+
+/*
 	for(int i=0; IP[i] != NULL; i++){
 		printf("%s\n", inet_ntoa(*IP[i])); // to convert the hostname into IP address and show the resolved IP address
 	}
-
+*/
 	saddr.sin_port = htons(port);
 
-	if(connect(sockfd, (struct sockaddr *) &saddr,sizeof(saddr)) < 0) {
+	if(connect(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
 		printf("Cannot connect \n");
+		close(sockfd);
+		return -1;
 	}
-
-	printf("Connected successfully \n");
+	else{
+		printf("Connected successfully \n");
+	}
+	
 	return 0;
 }
+
